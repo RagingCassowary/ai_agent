8768a003e49ae95a6f0d3646c07da4dce626d3c6
RagingCassowary <jakeob@live.co.uk>
2026-03-10 14:23:38 +0000

    2026.03.10: added metadata printout to main.py

--- main.py
+++ main.py
@@ -21,4 +21,9 @@ if __name__ == "__main__":
 response = client.models.generate_content(model="gemini-2.5-flash", 
                                           contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
                                           )
+if response.usage_metadata == None:
+    raise RuntimeError("no usage metadata")
+
+print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 print(response.text)
