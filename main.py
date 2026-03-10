c2ee3b4445f41270664c1818fbc0100064fd0921
RagingCassowary <jakeob@live.co.uk>
2026-03-10 14:35:59 +0000

    2026.03.10: added parser to main.py to parse user inputted prompts from command line

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -11,19 +12,22 @@ from google import genai
 
 client = genai.Client(api_key=api_key)
 
-def main():
-    print("Hello from ai-agent!")
-
-
-if __name__ == "__main__":
-    main()
+parser = argparse.ArgumentParser(description="AI Agent")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
 
 response = client.models.generate_content(model="gemini-2.5-flash", 
-                                          contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+                                          contents=args.user_prompt
                                           )
 if response.usage_metadata == None:
     raise RuntimeError("no usage metadata")
 
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-print(response.text)
+def main():
+    print("Hello from ai-agent!")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(response.text)
+
+
+if __name__ == "__main__":
+    main()
