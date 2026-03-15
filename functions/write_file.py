a338577f6ddcd10035464c65d919613abe5a7007
RagingCassowary <jakeob@live.co.uk>
2026-03-15 20:05:24 +0000

    2026.03.15: added test file for write_file function, fixed bug where pkg/more_lorem.txt was read as directory

--- functions/write_file.py
+++ functions/write_file.py
@@ -6,23 +6,19 @@ def write_file(working_dir, filepath, content):
         working_dir_abs = os.path.abspath(working_dir)
         target_file = os.path.normpath(os.path.join(working_dir_abs, filepath))
         valid_filepath = os.path.commonpath([working_dir_abs, target_file]) == working_dir_abs
-    except:
-        return f'Error: Could not validate filepath to "{filepath}"'
     
-    if not valid_filepath:
-        return f'Error: Cannot read "{filepath}" as it is outside the permitted working directory'
+        if not valid_filepath:
+            return f'Error: Cannot read "{filepath}" as it is outside the permitted working directory'
     
-    if os.path.isdir(target_file):
-        return f'Error: Cannot write to "{filepath}" as it is a directory'
+        if os.path.isdir(target_file):
+            return f'Error: Cannot write to "{filepath}" as it is a directory'
 
-    try: 
-        full_filepath = os.path.join(working_dir, filepath)
-        os.makedirs(full_filepath, exist_ok=True)
-    except:
-        return f'Error: Could not construct full filepath to "{filepath}"'
-
-
-    with open(full_filepath, "w") as f:
-        f.write(content)
+        os.makedirs(os.path.dirname(target_file), exist_ok=True)
     
-    return f'Successfully wrote to "{filepath}" ({len(content)} characters written)'
+        with open(target_file, "w") as f:
+            f.write(content)
+    
+        return f'Successfully wrote to "{filepath}" ({len(content)} characters written)'
+    
+    except Exception as e:
+        return f'Error: could not write to file: {e}'
