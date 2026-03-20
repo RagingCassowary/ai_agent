c7b614abd98bc147794cfe5472d4437abbdf7cd9
RagingCassowary <jakeob@live.co.uk>
2026-03-20 08:18:01 +0000

    2026.03.20: added looping logic for agent, so that functions can continue to be called until a text response is generated.

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import argparse
 from dotenv import load_dotenv
 from prompts import system_prompt
 from call_function import available_functions, call_function
+from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -10,54 +12,65 @@ api_key = os.environ.get("GEMINI_API_KEY")
 if api_key == None:
     raise RuntimeError("api key not found")
 
-from google import genai
-
 client = genai.Client(api_key=api_key)
 
-parser = argparse.ArgumentParser(description="AI Agent")
-parser.add_argument("user_prompt", type=str, help="User prompt")
-parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
-args = parser.parse_args()
+def main():
 
-from google.genai import types
+    parser = argparse.ArgumentParser(description="AI Agent")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
 
-messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
-response = client.models.generate_content(
-    model="gemini-2.5-flash", 
-    contents=messages, 
-    config=types.GenerateContentConfig(
-        tools=[available_functions], system_instruction=system_prompt
-        )
-    )
-if response.usage_metadata == None:
-    raise RuntimeError("no usage metadata")
+    for t in range(20):
+
+        response = client.models.generate_content(
+            model="gemini-2.5-flash", 
+            contents=messages, 
+            config=types.GenerateContentConfig(
+                tools=[available_functions], system_instruction=system_prompt
+                )
+            )
+        if response.usage_metadata == None:
+            raise RuntimeError("no usage metadata")
+        if len(response.candidates) > 0:
+            for candidate in response.candidates:
+                messages.append(candidate.content)
+
+
+        print("Hello from ai-agent!")
+        if args.verbose:
+            print(f"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        
+        if len(response.function_calls) > 0:
+            function_results = []
+            for function_call in response.function_calls:
+                function_call_result = call_function(function_call)
+                if len(function_call_result.parts) < 1 or len(function_call_result.parts) == None:
+                    raise Exception("invalid function result: no .parts list")
+                if function_call_result.parts[0].function_response == None:
+                    raise Exception("invalid function result: no function_response")
+                if function_call_result.parts[0].function_response.response == None:
+                    raise Exception("invalid function result: no function_response.response")
+                function_results.append(function_call_result.parts[0])
+                if args.verbose:
+                    print(f"-> {function_call_result.parts[0].function_response.response}")
+            messages.append(types.Content(role="user", parts=function_results))
+        
+        if len(response.function_calls) == 0:
+            print(response.text)
+            return
+        
+        if t == 20:
+            print("After 20 iterations, could not produce a final response.")
+            exit(1)
+
 
 def format_function(name, args):
     return f"Calling function: {name}({args})"
-
-def main():
-    print("Hello from ai-agent!")
-    if args.verbose:
-        print(f"User prompt: {args.user_prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    if len(response.function_calls) > 0:
-        function_results = []
-        for function_call in response.function_calls:
-            function_call_result = call_function(function_call)
-            if len(function_call_result.parts) < 1 or len(function_call_result.parts) == None:
-                raise Exception("invalid function result: no .parts list")
-            if function_call_result.parts[0].function_response == None:
-                raise Exception("invalid function result: no function_response")
-            if function_call_result.parts[0].function_response.response == None:
-                raise Exception("invalid function result: no function_response.response")
-            function_results.append(function_call_result.parts[0])
-            if args.verbose:
-                print(f"-> {function_call_result.parts[0].function_response.response}")
-    if len(response.function_calls) == 0:
-        print(response.text)
-
 
 if __name__ == "__main__":
     main()
